ff85990be2b49c0dacb98133e4e4bf1eff30bf9d
jeric realubit <jericrealubit@gmail.com>
2025-05-28 14:18:03 +1200

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
+</template>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div>Show</div>
+    <Link href="/">Main Page</Link>
+</template>
